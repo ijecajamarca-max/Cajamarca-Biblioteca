d3212dae47d9cbef16a25a41478beaeeca380465
ijecajamarca-max <ije.cajamarca@yavirac.edu.ec>
2026-08-21 17:36:00 -0500

    Refactor login component to use FormsModule

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
-  templateUrl: './login.html',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html'
 })
 export class Login {
+  correo = '';
+  password = '';
+  mensaje = '';
+  constructor(private router: Router) {}
+  iniciarSesion(): void {
+    if (this.correo === 'ije.cajamarca@yavirac.edu.ec' &&
+        this.password === '123456') {
+      this.router.navigate(['/inicio']);
+    } else {
+      this.mensaje = 'Correo o contraseña incorrectos';
+    }
+  }
 }
