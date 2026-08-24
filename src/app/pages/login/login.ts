2c339b0a45a13e4029fd58a5d6f3be5ff94e69d6
ijecajamarca-max <ije.cajamarca@yavirac.edu.ec>
2026-08-24 17:14:10 -0500

    Refactor login component for improved validation

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './login.html'
 })
 export class Login {
   correo = '';
   password = '';
   mensaje = '';
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router
+  ) {}
   iniciarSesion(): void {
-    if (this.correo === 'ije.cajamarca@yavirac.edu.ec' &&
-        this.password === '123456') {
+    this.mensaje = '';
+    const usuarioGuardado =
+      localStorage.getItem('usuario');
+    if (!usuarioGuardado) {
+      this.mensaje =
+        'No existe ninguna cuenta registrada.';
+      return;
+    }
+    const usuario = JSON.parse(usuarioGuardado);
+    if (
+      usuario.correo.toLowerCase() ===
+      this.correo.toLowerCase() &&
+      usuario.password === this.password
+    ) {
       this.router.navigate(['/inicio']);
     } else {
-      this.mensaje = 'Correo o contraseña incorrectos';
+      this.mensaje =
+        'El correo o la contraseña son incorrectos.';
     }
   }
 }
